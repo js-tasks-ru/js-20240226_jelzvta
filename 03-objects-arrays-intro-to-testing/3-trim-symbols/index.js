6b489671de579a2362292ca0f789face990b2d15
douxcode <jelzvta@yandex.ru>
2024-03-07 22:34:14 +0300

    03-3 trim symbols task

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,23 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  const removeDuplicates = (string, size) => {
+    let counter = 1;
+    const stringCollection = string.split('');
+    let trimmedArray = [stringCollection[0]];
+    for (let i = 1; i < stringCollection.length; i++) {
+      if (stringCollection[i] !== stringCollection[i - 1]) {
+        counter = 1;
+        trimmedArray.push(stringCollection[i]);
+      } else {
+        counter++;
+        if (counter <= size) {
+          trimmedArray.push(stringCollection[i]);
+        }
+      }
+    }
+    return trimmedArray.join('');
+  }
 
-}
+  return size === 0 ? '' : !size ? string : removeDuplicates(string, size);
+  }
