8d38231dc7002aef76308ba3d3767dc4a8b64220
douxcode <jelzvta@yandex.ru>
2024-03-17 09:44:35 +0300

    updated the column building function

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -8,19 +8,6 @@ export default class SortableTable {
     this.subElements = this.getSubElements(this.element);
   }
 
-  loadingTemplate() {
-    return `
-        <div data-element="loading" class="loading-line sortable-table__loading-line"></div>
-            <div data-element="emptyPlaceholder" class="sortable-table__empty-placeholder">
-            <div>
-              <p>No products satisfies your filter criteria</p>
-              <button type="button" class="button-primary-outline">Reset all filters</button>
-            </div>
-          </div>
-        </div>
-      `;
-  }
-
   getSubElements(element) {
     const result = {};
     const elements = element.querySelectorAll('[data-element]');
@@ -36,47 +23,47 @@ export default class SortableTable {
     const productsContainer = document.createElement('div');
     productsContainer.setAttribute('data-element', 'productsContainer');
     productsContainer.classList.add('products-list__container');
-    productsContainer.innerHTML =
-    `<div data-element="productsContainer" class="products-list__container">
-      <div class="sortable-table">
-        <div data-element="header" class="sortable-table__header sortable-table__row">
-          ${this.createCellHeaderTemplate()}
-        </div>
-        <div data-element="body" class="sortable-table__body">'
-          ${this.createCellTemplate()}
+    productsContainer.innerHTML = `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${this.createCellHeaderTemplate()}
+          </div>
+          <div data-element="body" class="sortable-table__body">
+            ${this.createBodyTemplate(this.data)}
+          </div>
         </div>
       </div>
-    </div>`;
+    `;
     return productsContainer;
   }
 
   createCellHeaderTemplate() {
     return this.headerConfig.map(({id, title, sortable}) => {
-      return `<div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
-        <span>${title}</span>
-        ${title === 'Name' ? `<span data-element="arrow" class="sortable-table__sort-arrow">
+      return `
+        <div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}">
+          <span>${title}</span>
+          ${title === 'Name' ? `<span data-element="arrow" class="sortable-table__sort-arrow">
             <span class="sort-arrow"></span>
           </span>` : ''}
-      </div>`;
+        </div>
+      `;
     }).join('');
   }
 
-  createCellTemplate() {
-    return this.data.map(({ id, title, images, quantity, price, sales }) => {
-      const imageCell = this.headerConfig.find(column => column.id === 'images');
-      const imageContent = imageCell ? imageCell.template({ url: images }) : `<img class="sortable-table-image" alt="Image" src="${images}">`;
+  createBodyTemplate(data) {
+    return data.map(rowData =>
+      `<div class="row">
+        ${this.headerConfig.map(config => this.createBodyColumnTemplate(config, rowData)).join('')}
+      </div>`
+    ).join('');
+  }
 
-      const cells = this.headerConfig.map(column => {
-        if (column.id === 'images') {
-          return `<div class="sortable-table__cell">${imageContent}</div>`;
-        } else {
-          const content = column.template ? column.template({ [column.id]: eval(column.id) }) : eval(column.id);
-          return `<div class="sortable-table__cell">${content}</div>`;
-        }
-      }).join('');
-
-      return `<a href="/products/${id}" class="sortable-table__row">${cells}</a>`;
-    }).join('');
+  createBodyColumnTemplate(config, rowData) {
+    if (config.template) {
+      return config.template(rowData);
+    }
+    return `<div class="sortable-table__cell">${rowData[config.id]}</div>`;
   }
 
   sort(fieldValue, optionValue) {
@@ -94,22 +81,18 @@ export default class SortableTable {
         return (keyA - keyB) * k;
       } else if (sortType === 'string') {
         return k * keyA.localeCompare(keyB, ['ru', 'en'], {caseFirst: 'upper'});
-      } else {
-        return 0;
       }
     });
     this.update(sortedData);
   }
 
-
   update(sortedData) {
     this.data = sortedData;
     const { body } = this.subElements;
-    body.innerHTML = this.createCellTemplate(sortedData);
+    body.innerHTML = this.createBodyTemplate(sortedData);
   }
 
   destroy() {
     this.element.remove();
   }
 }
-
