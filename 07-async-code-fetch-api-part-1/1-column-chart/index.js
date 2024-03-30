fbaf709be06373abf39079eb110e8d963de7c86a
douxcode <jelzvta@yandex.ru>
2024-03-30 16:37:44 +0300

    07-1 passed tests

--- 07-async-code-fetch-api-part-1/1-column-chart/index.js
+++ 07-async-code-fetch-api-part-1/1-column-chart/index.js
@@ -2,6 +2,37 @@ import fetchJson from './utils/fetch-json.js';
 
 const BACKEND_URL = 'https://course-js.javascript.ru';
 
-export default class ColumnChart {
+import ColumnChart from '../../04-oop-basic-intro-to-dom/1-column-chart/index.js';
+
+export default class ColumnChartV2 extends ColumnChart {
+    subElements = {};
+
+    constructor({ url = '', range = {}, label = '', link = '', formatHeading} = {}) {
+      super({ url, range, label, link, formatHeading });
+      this.url = url;
+      this.to = range.to;
+      this.from = range.from;
+      this.label = label;
+      this.link = link;
+      this.formatHeading = formatHeading;
+      this.getSubElements();
+    }
+
+    getSubElements () {
+      this.element.querySelectorAll('[data-element]').forEach((element) => {
+        this.subElements[element.dataset.element] = element;
+      });
+    }
+
+    async getData() {
+      const ordersData = await fetchJson(`${BACKEND_URL}/${this.url}`);
+      return ordersData;
+    }
+
+    async update(from, to) {
+      this.data = await this.getData();
+      this.subElements.body.innerHTML = this.getColumnBody(Object.values(this.data));
+      return this.data;
+    }
 
 }
