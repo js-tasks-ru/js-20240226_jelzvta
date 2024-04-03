fe02f442abc9a23f9626ebd85d304a2e28d0d2c7
douxcode <jelzvta@yandex.ru>
2024-04-03 19:01:19 +0300

    fixed options

--- 08-forms-fetch-api-part-2/1-product-form-v1/index.js
+++ 08-forms-fetch-api-part-2/1-product-form-v1/index.js
@@ -8,6 +8,7 @@ export default class ProductForm {
   element = document.createElement('div');
   subElements = {};
   product;
+  subcategories;
   constructor (productId = '') {
     this.productId = productId;
     this.element.classList.add('product-form');
@@ -28,17 +29,36 @@ export default class ProductForm {
         this.product = data[0];
       }
     }
+    const categoriesData = await fetchJson(`${BACKEND_URL}/api/rest/categories?_refs=subcategory`);
+    this.subcategories = categoriesData;
   }
 
   async render() {
     await this.fetchProductData();
-    const formTemplate = this.createFormTemplate(this.product);
+    const formTemplate = this.createFormTemplate(this.product, this.subcategories);
     this.element.innerHTML = formTemplate;
     return this.element;
   }
 
-  createFormTemplate() {
-    const {title, description, price, discount, quantity, status} = this.product;
+
+  getSubcategory(subcategories) {
+    let subCategoriesOptions = '';
+    subcategories.forEach(({subcategories, title: mainTitle}) => {
+      subcategories.forEach(({title: subTitle, id}) => {subCategoriesOptions += `<option value="${id}">${mainTitle} > ${subTitle}</option>`;});
+    });
+    return subCategoriesOptions;
+  }
+
+  createFormTemplate(product, subcategories) {
+    const productData = {
+      title: product.title || '',
+      description: product.description || '',
+      price: product.price || '',
+      discount: product.discount || '',
+      quantity: product.quantity || '',
+      status: product.status || '',
+    };
+    const {title, description, price, discount, quantity, status} = productData;
     return `<form data-element="productForm" class="form-grid">
       <div class="form-group form-group__half_left">
         <fieldset>
@@ -53,11 +73,11 @@ export default class ProductForm {
       <div class="form-group form-group__wide" data-element="sortable-list-container">
         <label class="form-label">Фото</label>
         <div data-element="imageListContainer"><ul class="sortable-list"><li class="products-edit__imagelist-item sortable-list__item" style="">
-          <input type="hidden" name="url" value="https://i.imgur.com/MWorX2R.jpg">
+          <input type="hidden" name="url" value="">
           <input type="hidden" name="source" value="75462242_3746019958756848_838491213769211904_n.jpg">
           <span>
         <img src="icon-grab.svg" data-grab-handle="" alt="grab">
-        <img class="sortable-table__cell-img" alt="Image" src="https://i.imgur.com/MWorX2R.jpg">
+        <img class="sortable-table__cell-img" alt="Image" src="">
         <span>75462242_3746019958756848_838491213769211904_n.jpg</span>
       </span>
           <button type="button">
@@ -67,46 +87,7 @@ export default class ProductForm {
       </div>
       <div class="form-group form-group__half_left">
         <label class="form-label">Категория</label>
-        <select class="form-control" name="subcategory">
-          <option value="progulki-i-detskaya-komnata">Детские товары и игрушки &gt; Прогулки и детская комната</option>
-          <option value="kormlenie-i-gigiena">Детские товары и игрушки &gt; Кормление и гигиена</option>
-          <option value="igrushki-i-razvlecheniya">Детские товары и игрушки &gt; Игрушки и развлечения</option>
-          <option value="aktivniy-otdyh-i-ulitsa">Детские товары и игрушки &gt; Активный отдых и улица</option>
-          <option value="radioupravlyaemye-modeli">Детские товары и игрушки &gt; Радиоуправляемые модели</option>
-          <option value="shkolnye-tovary">Детские товары и игрушки &gt; Школьные товары</option>
-          <option value="noutbuki-i-aksessuary">Компьютерная техника &gt; Ноутбуки и аксессуары</option>
-          <option value="monitory">Компьютерная техника &gt; Мониторы</option>
-          <option value="komplektuyuschie">Компьютерная техника &gt; Комплектующие</option>
-          <option value="setevoe-oborudovanie">Компьютерная техника &gt; Сетевое оборудование</option>
-          <option value="vstraivaemaya-tehnika">Крупная бытовая техника &gt; Встраиваемая техника</option>
-          <option value="stiralnye-mashiny">Крупная бытовая техника &gt; Стиральные машины</option>
-          <option value="sushilnye-mashiny">Крупная бытовая техника &gt; Сушильные машины</option>
-          <option value="holodilniki">Крупная бытовая техника &gt; Холодильники</option>
-          <option value="morozilnye-kamery">Крупная бытовая техника &gt; Морозильные камеры</option>
-          <option value="vinnye-shkafy">Крупная бытовая техника &gt; Винные шкафы</option>
-          <option value="vytyazhki">Крупная бытовая техника &gt; Вытяжки</option>
-          <option value="plity">Крупная бытовая техника &gt; Плиты</option>
-          <option value="posudomoechnye-mashiny">Крупная бытовая техника &gt; Посудомоечные машины</option>
-          <option value="melkaya-bytovaya-tehnika">Крупная бытовая техника &gt; Мелкая бытовая техника</option>
-          <option value="mikrovolnovye-pechi">Крупная бытовая техника &gt; Микроволновые печи</option>
-          <option value="elektroduhovki">Крупная бытовая техника &gt; Электродуховки</option>
-          <option value="uborochnye-mashiny">Крупная бытовая техника &gt; Уборочные машины</option>
-          <option value="paroochistiteli">Крупная бытовая техника &gt; Пароочистители</option>
-          <option value="kulery-i-purifayery">Крупная бытовая техника &gt; Кулеры и пурифайеры</option>
-          <option value="kuhnya">Мелкая бытовая техника &gt; Кухня</option>
-          <option value="bytovye-pribory-dlya-doma">Мелкая бытовая техника &gt; Бытовые приборы для дома</option>
-          <option value="krasota-i-gigiena">Мелкая бытовая техника &gt; Красота и гигиена</option>
-          <option value="lcd-televizory">ТВ и видеотехника &gt; LCD телевизоры</option>
-          <option value="podstavki-i-krepleniya">ТВ и видеотехника &gt; Подставки и крепления</option>
-          <option value="mediapleery">ТВ и видеотехника &gt; Медиаплееры</option>
-          <option value="tv-tyunery">ТВ и видеотехника &gt; ТВ тюнеры</option>
-          <option value="tv-antenny">ТВ и видеотехника &gt; ТВ антенны</option>
-          <option value="3d-ochki">ТВ и видеотехника &gt; 3D очки</option>
-          <option value="ochki-virtualnoy-realnosti">ТВ и видеотехника &gt; Очки виртуальной реальности</option>
-          <option value="proektsionnoe-oborudovanie">ТВ и видеотехника &gt; Проекционное оборудование</option>
-          <option value="videokamery-i-aksessuary">ТВ и видеотехника &gt; Видеокамеры и аксессуары</option>
-          <option value="dvd/blu-ray-pleery">ТВ и видеотехника &gt; DVD/Blu-ray плееры</option>
-        </select>
+        <select class="form-control" name="subcategory" id="subcategory">${this.getSubcategory(subcategories)}</select>
       </div>
       <div class="form-group form-group__half_left form-group__two-col">
         <fieldset>
