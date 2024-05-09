287b6718349f54a17dae90b78c751c0f84612f19
douxcode <jelzvta@yandex.ru>
2024-05-09 20:51:19 +0300

    add ability to remove item from list

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -15,5 +15,58 @@ export default class SortableList {
         item.classList.add('sortable-list__item');
         this.element.append(item);
       });
+      this.initEventListeners();
+    }
+
+    initEventListeners() {
+      const grabHandle = this.element.querySelectorAll('[data-grab-handle]');
+      grabHandle.forEach(el => el.addEventListener('pointerdown', this.onPointerDown));
+      const deleteHandle = this.element.querySelectorAll('[data-delete-handle]');
+      deleteHandle.forEach(el => el.addEventListener('pointerdown', this.onDeleteHandle));
+    }
+
+    onDeleteHandle = (event) => {
+      const deleteHandle = event.target.closest('[data-delete-handle]');
+      if (deleteHandle) {
+        deleteHandle.closest('.sortable-list__item').remove();
+      }
+    }
+
+    onPointerDown = (event) => {
+      const grabHandle = event.target.closest('[data-grab-handle]');
+      if (grabHandle) {
+        event.preventDefault();
+        this.draggingElement = grabHandle.closest('.sortable-list__item');
+        this.draggingElement.style.width = `${this.draggingElement.offsetWidth}px`;
+        this.draggingElement.style.height = `${this.draggingElement.offsetHeight}px`;
+        this.draggingElement.classList.add('sortable-list__item_dragging');
+        this.element.append(this.draggingElement);
+        this.moveAt(event);
+        document.addEventListener('pointermove', this.onPointerMove);
+        document.addEventListener('pointerup', this.onPointerUp);
+      }
+    }
+
+    onPointerMove = (event) => {
+      this.moveAt(event);
+    }
+
+    onPointerUp = () => {
+      this.draggingElement.style.cssText = '';
+      this.draggingElement.classList.remove('sortable-list__item_dragging');
+      document.removeEventListener('pointermove', this.onPointerMove);
+      document.removeEventListener('pointerup', this.onPointerUp);
+    }
+
+    moveAt(event) {
+      this.draggingElement.style.left = `${event.clientX - this.draggingElement.offsetWidth / 2}px`;
+      this.draggingElement.style.top = `${event.clientY - this.draggingElement.offsetHeight / 2}px`;
+    }
+
+    remove() {
+      this.element.remove();
+    }
+    destroy() {
+      this.remove();
     }
 }
