911828eda51a1e10840611038016b9f0a939d73b
douxcode <jelzvta@yandex.ru>
2024-05-09 20:26:42 +0300

    init 09-2

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,19 @@
 export default class SortableList {
-
+    element;
+    constructor({
+      data = [],
+      items = data => data
+    } = {}) {
+      this.data = data;
+      this.items = items;
+      this.render();
+    }
+    render() {
+      this.element = document.createElement('ul');
+      this.element.className = 'sortable-list';
+      this.items.forEach(item => {
+        item.classList.add('sortable-list__item');
+        this.element.append(item);
+      });
+    }
 }
