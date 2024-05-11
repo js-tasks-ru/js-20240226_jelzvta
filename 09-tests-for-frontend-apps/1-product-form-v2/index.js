3f0141361ee85bdf1037d1b6ef7d6cfdc12dfa79
douxcode <jelzvta@yandex.ru>
2024-05-11 17:32:55 +0300

    extend ProductForm class, render

--- 09-tests-for-frontend-apps/1-product-form-v2/index.js
+++ 09-tests-for-frontend-apps/1-product-form-v2/index.js
@@ -1,16 +1,16 @@
-import SortableList from '../../2-sortable-list/src/index.js';
 import escapeHtml from './utils/escape-html.js';
 import fetchJson from './utils/fetch-json.js';
 
 const IMGUR_CLIENT_ID = '28aaa2e823b03b1';
 const BACKEND_URL = 'https://course-js.javascript.ru';
+const API_PRODUCTS = '/api/rest/products';
+const API_CATEGORIES = '/api/rest/categories';
 
-export default class ProductForm {
-  constructor (productId) {
+import ProductForm from '../../08-forms-fetch-api-part-2/1-product-form-v1/index.js';
+
+export default class ProductFormV2 extends ProductForm {
+  constructor (productId = '') {
+    super(productId);
     this.productId = productId;
-  }
-
-  async render () {
-
   }
 }
